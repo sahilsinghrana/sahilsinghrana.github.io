9ddfb3b30fa0d7439cf5ecb5813c045d8f5c9433
Sahil Singh Rana <ranasahil98@gmail.com>
2024-05-25 02:02:56 +0530

    remove solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,3 @@
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({
-	// Enable Solid to support Solid JSX components.
-	integrations: [solid()],
-});
+export default defineConfig({});
