788570702457bf4ca437be63f54493ac793f5929
Sahil Singh Rana <ranasahil98@gmail.com>
2024-05-25 03:08:24 +0530

    the moooon

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,4 +6,5 @@ export const links = {
   blog: "/blog",
   github: "http://github.com/sahilsinghrana",
   linkedin: "https://www.linkedin.com/in/sahilsinghrana/",
+  profilePic: "https://avatars.githubusercontent.com/u/48375250?v=4",
 };
