d41c0e19dbbad050255e872bbe8ef7ae5ce66364
Sahil Singh Rana <ranasahil98@gmail.com>
2024-05-27 21:26:18 +0530

    Blog Card

--- src/content/blog/howto/how-to-convert-string-to-literal.md
+++ src/content/blog/howto/how-to-convert-string-to-literal.md
@@ -1,5 +1,14 @@
 ---
+# layout: ../../layouts/MarkdownPostLayout.astro
+author: "Sahil Singh Rana"
+image:
+  url: "https://docs.astro.build/assets/full-logo-light.png"
+  alt: "The Astro logo with the word One."
+pubDate: 2024-05-28
 title: How to conver string to string literal.
-# slug: my-custom-slug/supports/slashes
+description: "This is the first post of my new Astro blog."
+slug: my-custom-slug/supports/slashes
+tags: ["astro", "blogging", "learning in public"]
 ---
+
 Your blog post content here.
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,20 @@
-// 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
-// 2. Define your collection(s)
-const blogCollection = defineCollection({ /* ... */ });
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+
 export const collections = {
-  'blog': blogCollection,
+  blog: blogCollection,
 };
