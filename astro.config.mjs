8caa34f6e2467a246efd2d6c1cf43a84ba103c07
Sahil Singh Rana <ranasahil98@gmail.com>
2024-05-27 22:39:11 +0530

    remove comp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import playformCompress from "@playform/compress";
-
-// https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   site: "https://sahilsinghrana.github.io",
-  integrations: [sitemap(), playformCompress()],
+  integrations: [sitemap()],
   prefetch: {
-    defaultStrategy: "viewport"
-  }
+    defaultStrategy: "viewport",
+  },
 });
