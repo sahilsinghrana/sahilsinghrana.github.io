0a51574f8e8928c3f027ff0b9cf284cd24c7d63b
Sahil Rana <ranasahil98@gmail.com>
2024-06-07 12:57:44 +0530

    fix: links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
 
 export const links = {
   home: "/",
-  about: "/about",
-  contact: "/contact",
-  projects: "/projects",
-  blog: "/blog",
+  about: "/about/",
+  contact: "/contact/",
+  projects: "/projects/",
+  blog: "/blog/",
   github: "http://github.com/sahilsinghrana",
   linkedin: "https://www.linkedin.com/in/sahilsinghrana/",
   profilePic: "https://avatars.githubusercontent.com/u/48375250?v=4",
