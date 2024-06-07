ca9436bb453a011e5014d77a6215920b51cc5987
Sahil Rana <ranasahil98@gmail.com>
2024-06-07 11:33:24 +0530

    feat: partytown and enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-
 import playformInline from "@playform/inline";
 import playformCompress from "@playform/compress";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   site: "https://sahilsinghrana.github.io",
-  integrations: [sitemap(), playformInline(), playformCompress(), compressor()],
+  integrations: [partytown(), sitemap(), playformInline(), playformCompress(), compressor()],
   prefetch: {
-    defaultStrategy: "viewport",
+    defaultStrategy: "viewport"
   },
   markdown: {
     shikiConfig: {
-      theme: "github-dark",
-    },
-  },
+      theme: "github-dark"
+    }
+  }
 });
