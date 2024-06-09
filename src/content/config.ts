9e1bf8b3596c06a5cb9517e2a35b45f257c8591f
Sahil Singh Rana <ranasahil98@gmail.com>
2024-06-10 00:36:01 +0530

    blog:snippets

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,19 +2,35 @@ import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    author: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      author: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      image: z.object({
+        url: image().or(z.string()),
+        alt: z.string(),
+      }),
+      tags: z.array(z.string()),
     }),
-    tags: z.array(z.string()),
-  }),
+});
+
+const snippetsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      image: z.object({
+        url: image().or(z.string()),
+        alt: z.string(),
+      }),
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
   blog: blogCollection,
+  snippets: snippetsCollection,
 };
