6bb2a2ebb720a9d69f9200acc393a0d9e8a7e4fe
sahilsinghrana <ranasahil98@gmail.com>
2024-08-11 00:27:18 +0530

    fix: breadcrumbs

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,10 +1,7 @@
-// @ts-nocheck
 module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-url"),
-    require("postcss-color-rgba-fallback"),
-    require("postcss-combine-media-query"),
     require("postcss-combine-duplicated-selectors")({
       removeDuplicatedProperties: false,
       removeDuplicatedValues: true,
