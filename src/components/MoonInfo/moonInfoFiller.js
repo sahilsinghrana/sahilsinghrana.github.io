8fb43eb14956751ef65e8b1160c2b15d28d57a39
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2024-08-23 16:38:32 +0530

    fix moon age percent

--- src/components/MoonInfo/moonInfoFiller.js
+++ src/components/MoonInfo/moonInfoFiller.js
@@ -6,7 +6,7 @@ const isWaning = Moon.isWaning();
 const lunarAge = Moon.lunarAge().toFixed(2).concat(" Days");
 const emojiForLunarPhase = Moon.emojiForLunarPhase(phase).concat(" ", phase);
 const lunarDistance = Moon.lunarDistance().toFixed(2);
-const lunarAgePercent = Moon.lunarAgePercent().toFixed(2).concat("%");
+const lunarAgePercent = (Moon.lunarAgePercent() * 100).toFixed(2).concat("%");
 
 const waxWaneText = isWaxing ? "Waxing" : isWaning ? "Waning" : "";
 
