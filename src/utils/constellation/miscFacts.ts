2a703bad6382b427a4e9d96494c29d2a75f53bbc
sahilsinghrana <ranasahil98@gmail.com>
2025-08-11 10:47:09 +0530

    random

---
+++ src/utils/constellation/miscFacts.ts
@@ -0,0 +1,25 @@
+import Fact from "@utils/Fact";
+
+const miscFacts = [
+  new Fact("Mars has has two moons : Phobos (“fear”) and Deimos (“terror”)"),
+  new Fact(
+    "Seasonal changes on Mars are more extreme due to its elliptical orbit",
+  ),
+  new Fact(
+    "Saturn has more than 140 moons, the most of any planet in our solar system",
+  ),
+  new Fact(
+    "Saturn's rings are made of ice, rock, and dust — beautiful yet fragile, symbolizing structure that can both protect and confine.",
+  ),
+  new Fact(
+    "When Mars and Venus appear close together in the night sky, it's called a planetary conjunction; a perspective alignment from Earth's viewpoint. Despite seeming near, they can still be tens of millions of kilometers apart in space.",
+  ),
+  new Fact(
+    "Aries lies along the ecliptic in the northern celestial hemisphere, while Scorpius dominates the southern sky. Because of their positions, they can never be overhead at the same time in most locations on Earth.",
+  ),
+  new Fact(
+    "Leo is a small, bright constellation, while Virgo is large and faint, making their observational qualities very different.",
+  ),
+];
+
+export default miscFacts;
--- src/utils/constellation/randomFactGenerator.ts
+++ src/utils/constellation/randomFactGenerator.ts
@@ -1,16 +1,13 @@
 import { getRandom } from "@components/Spotify/helpers/utils";
 
 import sagittariusFacts from "./sagittariusFacts";
+import miscFacts from "./miscFacts";
 
 export default function getRandomFact(): string {
-  const allFacts: string[] = [];
+  const allFacts: string[] = [...miscFacts.map((fact) => fact.fact)];
 
   Object.values(sagittariusFacts).forEach((fact) => {
-    allFacts.push(
-      fact.fact.startsWith("Sagittarius")
-        ? fact.fact
-        : "Sagittarius: " + fact.fact,
-    );
+    allFacts.push(fact.fact);
   });
 
   const randomIndex = getRandom(0, Object.keys(allFacts).length - 1);
