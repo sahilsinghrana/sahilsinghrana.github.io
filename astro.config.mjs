d507dac5597938a3f3988ce090bb8fc2c5f9a902
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2026-04-06 13:06:56 +0000

    fix: fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import purgecss from "astro-purgecss";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
 import compressor from "astro-compressor";
-
 import playformInline from "@playform/inline";
 
 import playformCompress from "@playform/compress";
@@ -15,6 +14,28 @@ export default defineConfig({
   site: "https://sahilrana.in",
   build: {
     format: "file",
+  },
+  image: {
+    config: {
+      webp: {
+        effort: 6,
+        quality: 50,
+        alphaQuality: 50,
+      },
+      avif: {
+        effort: 6,
+        quality: 40,
+        chromaSubsampling: "4:2:0",
+      },
+      jpeg: {
+        mozjpeg: true,
+        quality: 50,
+      },
+      png: {
+        compressionLevel: 9,
+        // palette: true,
+      },
+    },
   },
   vite: {
     build: {
@@ -61,4 +82,117 @@ export default defineConfig({
       ],
     ],
   },
+  fonts: [
+    {
+      name: "Manrope",
+      provider: fontProviders.local(),
+      cssVariable: "--font-manrope",
+      options: {
+        display: "swap",
+        variants: [
+          {
+            weight: "200",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/ManropeExtraLight-Regular.woff2",
+              "./src/assets/fonts/ManropeExtraLight-Regular.woff",
+              "./src/assets/fonts/Manrope-Regular.ttf",
+            ],
+          },
+          {
+            weight: "600",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/ManropeExtraLight-SemiBold.woff2",
+              "./src/assets/fonts/ManropeExtraLight-SemiBold.woff",
+            ],
+          },
+          {
+            weight: "800",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/ManropeExtraLight-ExtraBold.woff2",
+              "./src/assets/fonts/ManropeExtraLight-ExtraBold.woff",
+            ],
+          },
+        ],
+      },
+    },
+    {
+      name: "Raleway",
+      provider: fontProviders.local(),
+      cssVariable: "--font-raleway",
+      options: {
+        display: "swap",
+        variants: [
+          {
+            weight: "400",
+            style: "normal",
+            src: ["./src/assets/fonts/Raleway-Regular.ttf"],
+          },
+        ],
+      },
+    },
+    {
+      name: "Roboto",
+      provider: fontProviders.local(),
+      cssVariable: "--font-roboto",
+      options: {
+        display: "swap",
+        variants: [
+          {
+            weight: "400",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/Roboto-Regular.woff2",
+              "./src/assets/fonts/Roboto-Regular.woff",
+              "./src/assets/fonts/Roboto-Regular.ttf",
+            ],
+          },
+        ],
+      },
+    },
+    {
+      name: "Inter",
+      provider: fontProviders.local(),
+      cssVariable: "--font-inter",
+      options: {
+        display: "swap",
+        variants: [
+          {
+            weight: "200",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/Inter-ExtraLight.woff2",
+              "./src/assets/fonts/Inter-ExtraLight.woff",
+            ],
+          },
+          {
+            weight: "400",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/Inter-Regular.woff2",
+              "./src/assets/fonts/Inter-Regular.woff",
+            ],
+          },
+          {
+            weight: "600",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/Inter-SemiBold.woff2",
+              "./src/assets/fonts/Inter-SemiBold.woff",
+            ],
+          },
+          {
+            weight: "800",
+            style: "normal",
+            src: [
+              "./src/assets/fonts/Inter-ExtraBold.woff2",
+              "./src/assets/fonts/Inter-ExtraBold.woff",
+            ],
+          },
+        ],
+      },
+    },
+  ],
 });
