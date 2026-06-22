03ae94f354f449513ec68aed12e8d557019817d2
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2026-06-22 07:33:16 +0000

    resources

---
+++ src/components/Resources/elementInfos.ts
@@ -0,0 +1,33 @@
+import Moon from "./moon2.gif";
+import Cat from "./cat.gif";
+import Sunflower from "./sunflower.gif";
+import Tulip from "./tulip.gif";
+import Flute from "./flute.gif";
+
+export const ELEMENTS = [
+  {
+    img: Moon,
+    title: "Moon",
+    description: `So far away, yet so close, Comes in phases and always glows.`,
+  },
+  {
+    img: Cat,
+    title: "Cat",
+    description: `Soft and sharp, fierce and gentle, eyes so attractive,`,
+  },
+  {
+    img: Sunflower,
+    title: "Sunflower",
+    description: ` Blooms under the bright sun, dulls down in the dark,`,
+  },
+  {
+    img: Tulip,
+    title: "Tulip",
+    description: `Gentle, Calm, Colorful`,
+  },
+  {
+    img: Flute,
+    title: "Flute",
+    description: `Her voice so sweet, A therapy for the heart.`,
+  },
+];
