8a6aef4260d7351ea307982d624c8a5073ea8548
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2026-06-24 07:34:03 +0000

    resources

--- src/components/Resources/elementInfos.ts
+++ src/components/Resources/elementInfos.ts
@@ -1,33 +1,75 @@
 import Moon from "./moon2.gif";
-import Cat from "./cat.gif";
+import Cat from "./cat2.webp";
 import Sunflower from "./sunflower.gif";
 import Tulip from "./tulip.gif";
 import Flute from "./flute.gif";
+import Archer from "./archer.gif";
 
 export const ELEMENTS = [
   {
     img: Moon,
-    title: "Moon",
-    description: `So far away, yet so close, Comes in phases and always glows.`,
+    name: "Moon",
+    title: "The Moon",
+    shortDesc: `So far away, looks so close, 
+    Comes in phases yet always glows.`,
+
+    description: `There's something it attracts,
+    An effortless pull, a magnetic grace,
+    one look brings a smile to the face.
+    
+    Chase it down, it slips from view,
+    A silent guardian of the dark,
+    walks the miles along with you.`,
   },
   {
     img: Cat,
-    title: "Cat",
-    description: `Soft and sharp, fierce and gentle, eyes so attractive,`,
+    name: "Cat",
+    title: "The Cat",
+    shortDesc: "Eyes so magnetic, they hold you in place.",
+    description: `Soft and sharp, fierce and gentle,
+    moving with a quiet, deliberate grace.
+    Her body is soft, her hair is so cozy,
+    But don't be fooled by how sweet she acts,
+    out of nowhere comes her sharp attacks.`,
   },
   {
     img: Sunflower,
-    title: "Sunflower",
-    description: ` Blooms under the bright sun, dulls down in the dark,`,
+    name: "Sunflower",
+    title: "The Sunflower",
+    shortDesc:
+      "A loyal follower of the light, standing tall, proud and bright.",
+    description: `Blooms under the bright sun, dulls down in the dark,
+    A golden giant leaving its vibrant mark,
+    shining for you when the winds start to blow.
+    Because even when its bright yellow petals start to fall,
+    The happy memories it brought will stay through it all.`,
   },
   {
     img: Tulip,
-    title: "Tulip",
-    description: `Gentle, Calm, Colorful`,
+    name: "Tulip",
+    title: "The Tulip",
+    shortDesc:
+      "Gentle, Calm, Colorful, Like the soft lines that frame a happy smile.",
+    description: `A simple shape, yet wonderfully bold,
+    Bringing a beauty that always feels new,
+    Painting the earth in every bright hue.`,
   },
   {
     img: Flute,
-    title: "Flute",
-    description: `Her voice so sweet, A therapy for the heart.`,
+    name: "Flute",
+    title: "The Flute",
+    shortDesc: "Her voice so sweet, A therapy for the heart.",
+    description: `Carried by the wind,
+    With so much charm in every single part,
+    turning the heavy darkness into light,
+    the most beautiful sound you have ever heard.`,
+  },
+  {
+    img: Archer,
+    name: "Archer",
+    title: "The Sagittarius",
+    shortDesc: "The very center of the galaxy, shining bright.",
+    description: `The bow is drawn, but it seeks no distant spark,
+    because the presence is the only final mark.`,
   },
 ];
