b0f1b9703a7de9966391721fd7b0322f24126c19
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2026-06-25 11:15:58 +0000

    Chessdotcom widget init

---
+++ scripts/chessdotcom.ts
@@ -0,0 +1,52 @@
+export const CHESS_USERNAME = "sahilsinghrana";
+const ChessDotComBaseUrl = `https://api.chess.com/pub/player/${CHESS_USERNAME}`;
+
+export async function getPlayerProfile() {
+  try {
+    const res = await fetch(ChessDotComBaseUrl).then((res) => res.json());
+    return res;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getPlayerStats() {
+  try {
+    const res = await fetch(
+      ChessDotComBaseUrl + "/player/sahilsinghrana/stats",
+    ).then((res) => res.json());
+
+    return res;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getPlayerLatestMonthGameArchive() {
+  try {
+    const res = await fetch(ChessDotComBaseUrl + "/games/archives").then(
+      (res) => res.json(),
+    );
+    if (!Array.isArray(res.archives) || !res.archives.length) return;
+
+    const latestLink = res.archives.at(-1);
+
+    if (!latestLink) return;
+
+    const latestGames = await fetch(latestLink).then((res) => res.json());
+
+    if (!latestGames || !Array.isArray(latestGames.games)) return;
+
+    return latestGames?.games;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getLastFiveGames() {
+  const latestGames = await getPlayerLatestMonthGameArchive();
+
+  if (!latestGames) return;
+
+  return latestGames.slice(-5, latestGames.length).reverse();
+}
