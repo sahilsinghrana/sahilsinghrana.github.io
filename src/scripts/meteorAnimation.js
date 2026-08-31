a6bbd553fa0010736d096cce36f331aba1c7e61b
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2026-08-31 12:49:57 +0000

    fix: meteor positioning

--- src/scripts/meteorAnimation.js
+++ src/scripts/meteorAnimation.js
@@ -6,30 +6,14 @@ export function initMeteorAnimation() {
 
   const meteorsFragment = document.createDocumentFragment();
 
-  for (let i = 0; i <= 28; i++) {
+  for (let i = 0; i <= 35; i++) {
     const meteorDiv = document.createElement("div");
     meteorDiv.className = "meteor";
 
-    const spawnFromTop = Math.random() > 0.5;
-
-    let left, top;
-
-    if (spawnFromTop) {
-      left = Math.floor(getRandom(-50, 100));
-      top = -30;
-      meteorDiv.style.top = top + "px";
-      meteorDiv.style.left = left + "%";
-    } else {
-      left = -30;
-      top = Math.floor(getRandom(0, 100));
-      meteorDiv.style.left = left + "px";
-      meteorDiv.style.top = top + "%";
-    }
-
-    const delay = i === 0 ? 0.4 : getRandom(0, 50).toFixed(2);
-    const width = getRandom(45, 340);
+    const delay = i < 2 ? 0.4 : getRandom(-15, 50).toFixed(2);
+    const width = getRandom(48, 350);
     const height = getRandom(0.15, 3.1, 0.1);
-    const duration = 20 - ((width - 45) / (340 - 45)) * (20 - 6);
+    const duration = 20 - ((width - 48) / (350 - 48)) * (20 - 5);
 
     meteorDiv.style.setProperty("--duration", `${duration.toFixed(0)}s`);
     meteorDiv.style.setProperty("--width", `${width}px`);
@@ -37,8 +21,25 @@ export function initMeteorAnimation() {
     meteorDiv.style.setProperty("--delay", `${delay}s`);
     meteorDiv.style.setProperty(
       "--max-opacity",
-      String(getRandom(0.35, 0.58, 0.01)),
+      String(getRandom(0.3, 0.75, 0.01)),
     );
+
+    const spawnFromTop = Math.random() > 0.5;
+
+    let left, top;
+
+    if (spawnFromTop) {
+      left = Math.floor(getRandom(-25, 90));
+      top = 0 - width;
+      meteorDiv.style.top = top + "px";
+      meteorDiv.style.left = left + "%";
+    } else {
+      left = 0 - width;
+      meteorDiv.style.left = left + "px";
+
+      top = Math.floor(getRandom(-10, 90));
+      meteorDiv.style.top = top + "%";
+    }
 
     meteorsFragment.appendChild(meteorDiv);
   }
