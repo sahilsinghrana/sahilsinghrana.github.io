f125b389f538315955d0b86e29cf6f226cbebe16
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2026-08-31 13:45:41 +0000

    fixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -94,12 +94,6 @@ class SHIVA_Shloka extends Shloka {
 
 export const BHAGAVAD_GITA_SHLOKAS: Shloka[] = [
   new BG_Shloka(
-    3.8,
-    "नियतं कुरु कर्म त्वं कर्म ज्यायो ह्यकर्मणः। \n शरीरयात्रापि च ते न प्रसिद्ध्येदकर्मणः॥",
-    "Perform your bound duty. Action is better than inaction; without action, even the maintenance of your body is impossible.",
-  ),
-
-  new BG_Shloka(
     2.47,
     "कर्मण्येवाधिकारस्ते मा फलेषु कदाचन ।\nमा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्मणि ॥",
     "You have the right to perform your prescribed duties, but you are not entitled to the fruits of your actions. Never consider yourself the cause of the results of your activities, nor be attached to inaction.",
@@ -108,11 +102,6 @@ export const BHAGAVAD_GITA_SHLOKAS: Shloka[] = [
     3.5,
     "न हि कश्चित् क्षणमपि जातु तिष्ठत्यकर्मकृत् ।\nकार्यते ह्यवशः कर्म सर्वः प्रकृतिजैर्गुणैः ॥",
     "No one can ever remain without performing action even for a moment; for everyone is helplessly driven to action by the qualities born of nature..",
-  ),
-  new BG_Shloka(
-    2.16,
-    "नासतो विद्यते भावो नाभावो विद्यते सतः ।\nउभयोरपि दृष्टोऽन्तस्त्वनयोस्तत्त्वदर्शिभिः ॥",
-    "The unreal never comes into existence, and the Real never ceases to be. The ultimate truth about both these has been perceived by the seers of the Truth.",
   ),
   new BG_Shloka(
     2.63,
@@ -130,93 +119,24 @@ export const BHAGAVAD_GITA_SHLOKAS: Shloka[] = [
     "One who is not envious of any living entity, who is friendly and compassionate, who is free from the sense of proprietorship and false ego, who is equal in both happiness and distress, and who is always forgiving, he is very dear to Me.",
   ),
   new BG_Shloka(
-    5.1,
-    "ब्रह्मण्याधाय कर्माणि सङ्गं त्यक्त्वा करोति यः। लिप्यते न स पापेन पद्मपत्रमिवाम्भसा॥",
-    "One who performs his duty without attachment, surrendering the results to the Supreme, is untouched by sin, just as a lotus leaf is untouched by water",
-  ),
-  new BG_Shloka(
     18.17,
     "यस्य नाहङ्कृतो भावो बुद्धिर्यस्य न लिप्यते। हत्वापि स इमाँल्लोकान्न हन्ति न निबध्यते॥",
     "One who is free from the egoistic notion (that 'I am the doer') and whose intellect is not tainted, even if he slays these living beings, he neither slays nor is he bound by the action.",
-  ),
-  new BG_Shloka(
-    17.3,
-    "सत्त्वानुरूपा सर्वस्य श्रद्धा भवति भारत। श्रद्धामयोऽयं पुरुषो यो यच्छ्रद्धः स एव सः॥",
-    "The faith of every person, O Arjuna, is in accordance with their inner nature. A person is made of his faith; whatever his faith is, that verily he is.",
-  ),
-
-  new BG_Shloka(
-    6.26,
-    "यतो यतो निश्चरति मनश्चञ्चलमस्थिरम् ।\nततस्ततो नियम्यैतदात्मन्येव वशं नयेत् ॥",
-    "From wherever the mind wanders due to its flickering and unsteady nature, one must certainly withdraw it and bring it back under the control of the Self.",
   ),
   new BG_Shloka(
     3.27,
     "प्रकृतेः क्रियमाणानि गुणैः कर्माणि सर्वशः ।\nअहङ्कारविमूढात्मा कर्ताहमिति मन्यते ॥",
     "The spirit soul bewildered by the influence of false ego thinks himself the doer of activities that are in actuality carried out by the three modes of material nature.",
   ),
-  new BG_Shloka(
-    2.71,
-    "विहाय कामान्यः सर्वान्पुमांश्चरति निःस्पृहः ।\nनिर्ममो निरहङ्कारः स शान्तिमधिगच्छति ॥",
-    "That person, who gives up all material desires and lives free from a sense of greed, proprietorship, and egoism, attains perfect peace.",
-  ),
-  new BG_Shloka(
-    16.4,
-    "दम्भो दर्पोऽभिमानश्च क्रोध: पारुष्यमेव च | \nअज्ञानं चाभिजातस्य पार्थ सम्पदमासुरीम् ॥",
-    "O Parth, the qualities of those who possess a demoniac nature are hypocrisy, arrogance, conceit, anger, harshness, and ignorance.",
-  ),
 ];
 
 export const SHIVA_SHLOKAS: Shloka[] = [
-  new SHIVA_Shloka(
-    "Canto 3",
-    "तपःशक्त्या शरीरं निःसारं कृतवान् हरः । \n कामोऽभवन्मनो यस्य तस्य नास्ति पराजयः ॥",
-    "By the power of austerity, Hara made his body beyond material attraction. For whom desire itself becomes the mind, there can be no defeat.",
-    "Kumārasambhavam",
-  ),
-  new SHIVA_Shloka(
-    "18",
-    "न मे देहसुखे रागो न लोके नामहेषणा । \nn यः सुखं ब्रह्मरूपं तत्सुखं मम शाश्वतम् ॥",
-    "I have no attachment to bodily pleasure nor to worldly fame; my bliss lies in the Brahmic self;  that alone is eternal.",
-    "Śiva Purāṇa, Rudra Saṁhitā",
-  ),
-  new SHIVA_Shloka(
-    "2",
-    "नित्यं संन्यस्तभावोऽसौ नित्यवैराग्यधर्मवान् । \n स एव शक्त्या सायुज्यं प्राप साक्षात्परं शिवः ॥",
-    "Ever detached in nature, embodying the eternal law of renunciation, He, united with Śakti, became the supreme Śiva.",
-    "Skanda Purāṇa, Uma-Māhātmya",
-  ),
-  new SHIVA_Shloka(
-    "",
-    "वैराग्यमेव तेजोऽस्य न तु हठयोगसाधनम् । \nयेन कामोऽपि दग्धः स्यात् तं वन्दे योगिनां पतिम् ॥",
-    "His brilliance is his vairāgya, not forceful discipline; He who burnt even Desire; I bow to that Lord of Yogis.",
-    "Bṛhaspati Smriti",
-  ),
-  new SHIVA_Shloka(
-    7.104,
-    "उत्साहो बलवानार्यं नास्त्युत्साहात्परं बलम्। \n सोत्साहस्य हि लोकेषु न किञ्चिदपि दुर्लभम्॥",
-    "Enthusiasm is power itself; there is no strength greater than zeal. To one who acts with enthusiasm, nothing is truly difficult.",
-    "Manusmṛti",
-  ),
-
-  new SHIVA_Shloka(
-    177.25,
-    "उत्साहाद्यत्र सिद्ध्यन्ति कार्याणि न विपश्चितः। \n न तत्र शास्त्रं न बुद्धिर्न दक्षता न चौषधम्॥",
-    "Where zeal prevails, tasks succeed - not by scriptural learning, intellect, skill, or even medicine.",
-    "Mahābhārata, Śānti Parva",
-  ),
-  new SHIVA_Shloka(
-    "3",
-    "न मे द्वेषरागौ न मे लोभमोहौ । \n मदो नैव मे नैव मात्सर्यभावः ॥",
-    "I have no hatred or attachment, no greed or delusion; I have no arrogance, nor any feelings of jealousy. (I am Consciousness and Bliss, I am Shiva).",
-    "Nirvāṇa Shaṭakam",
-  ),
-  new SHIVA_Shloka(
-    "144",
-    "न पूजां न विसर्जनं न मन्त्रं न च चिन्तनम् । \n क्षोभादिपरमानन्दं जायते तन्मयं पदम् ॥",
-    "Neither ritual worship, nor dismissal, nor mantra, nor contemplation... When the mind is absorbed in the supreme bliss found in the center of agitation (stillness within chaos), that is the state of Shiva.",
-    "Vijñāna Bhairava Tantra",
-  ),
+  // new SHIVA_Shloka(
+  //   "Canto 3",
+  //   "तपःशक्त्या शरीरं निःसारं कृतवान् हरः । \n कामोऽभवन्मनो यस्य तस्य नास्ति पराजयः ॥",
+  //   "By the power of austerity, Hara made his body beyond material attraction. For whom desire itself becomes the mind, there can be no defeat.",
+  //   "Kumārasambhavam",
+  // ),
 ];
 
 const ALL_SHLOKAS: Shloka[] = [...SHIVA_SHLOKAS, ...BHAGAVAD_GITA_SHLOKAS];
